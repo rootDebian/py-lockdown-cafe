ed6cf7c8f6a5d8dde478807b287a1bd4e73ce5e8
Oleksii Kryvenkov <askryvenkov@gmail.com>
2025-03-31 19:04:57 +0300

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -10,22 +10,21 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: Dict) -> str:
+        name = visitor["name"]
 
         if "vaccine" not in visitor:
-            name = visitor["name"]
             raise NotVaccinatedError(f"{name} is not vaccinated.")
 
         expiration_date = visitor["vaccine"]["expiration_date"]
         if not isinstance(expiration_date, date):
             raise ValueError("Expiration date must be a datetime.date object.")
         if expiration_date < date.today():
-            raise OutdatedVaccineError(f"{visitor["name"]}'s"
-                                       f" vaccine expired on"
-                                       f" {expiration_date}.")
+            raise OutdatedVaccineError(f"{name}\"s "
+                                       f"vaccine expired"
+                                       f" on {expiration_date}.")
 
         wearing_a_mask = visitor.get("wearing_a_mask", False)
         if not wearing_a_mask:
-            raise NotWearingMaskError(f"{visitor["name"]}"
-                                      f" is not wearing a mask.")
+            raise NotWearingMaskError(f"{name} is not wearing a mask.")
 
         return f"Welcome to {self.name}"
