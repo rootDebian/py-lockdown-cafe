5491d96d27a7f7da7c54b8cc8eb7d7ea407af838
Oleksii Kryvenkov <askryvenkov@gmail.com>
2025-03-31 19:00:35 +0300

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -10,12 +10,11 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: Dict) -> str:
-        """Проверяет, может ли посетитель войти в кафе."""
-        # Проверка наличия вакцины
-        if "vaccine" not in visitor:
-            raise NotVaccinatedError(f"{visitor["name"]} is not vaccinated.")
 
-        # Проверка срока действия вакцины
+        if "vaccine" not in visitor:
+            name = visitor["name"]
+            raise NotVaccinatedError(f"{name} is not vaccinated.")
+
         expiration_date = visitor["vaccine"]["expiration_date"]
         if not isinstance(expiration_date, date):
             raise ValueError("Expiration date must be a datetime.date object.")
@@ -24,11 +23,9 @@ class Cafe:
                                        f" vaccine expired on"
                                        f" {expiration_date}.")
 
-        # Проверка маски
         wearing_a_mask = visitor.get("wearing_a_mask", False)
         if not wearing_a_mask:
             raise NotWearingMaskError(f"{visitor["name"]}"
                                       f" is not wearing a mask.")
 
-        # Если все проверки пройдены
         return f"Welcome to {self.name}"
